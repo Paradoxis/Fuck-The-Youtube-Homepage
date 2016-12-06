6779308a26affaca959f76bbae98b7d16785e032
Paradoxis <luke@paradoxis.nl>
2016-12-06 02:01:48 +0100

    Fixed the channel link, why the hell would I want to go to Google+ when clicking my channel icon?

--- src/content.js
+++ src/content.js
@@ -1,3 +1,41 @@
 (function() { "use strict";
-    document.querySelector("#yt-masthead a").href = "https://www.youtube.com/feed/subscriptions";
-})();
+
+    /**
+     * Changes the home button to default to the subscriptions page 
+     * @returns {void}
+     */
+    function changeHomeButtonLink()
+    {
+        document.querySelector("#yt-masthead a").href = "https://www.youtube.com/feed/subscriptions";
+    }
+
+    /**
+     * Aint' nobody got time for Google+
+     * @returns {void}
+     */
+    function changeChannelIconLink()
+    {
+        document.querySelector(".yt-masthead-picker-photo-wrapper").href = getMyChannelUrl();
+    }
+
+    /**
+     * Get the channel url belonging to the current user
+     * @returns {String}
+     */
+    function getMyChannelUrl()
+    {
+        return document.querySelectorAll(".guide-user-links a")[1].href;
+    }
+
+    /**
+     * Main entry point of the script 
+     * @returns {void}
+     */
+    function main()
+    {
+        changeHomeButtonLink();
+        changeChannelIconLink();
+    }
+    
+    return main;
+})()();
